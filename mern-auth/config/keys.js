5ba82a665fa3d3dbaeab1b9ac345b85705034239
Spencer <sjones97@email.unc.edu>
2020-12-17 22:45:09 -0500

    finish backend and start frontend

---
+++ mern-auth/config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: "mongodb+srv://new-user:Secret@cluster0.u8h7m.mongodb.net/users?retryWrites=true&w=majority",
+    secretOrKey: "secret"
+};
---
+++ mern-auth/server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+
+const users = require("./routes/api/users");
+
+const app = express();
+
+// Bodyparser Middleware
+app.use(
+    bodyParser.urlencoded({
+        extended: false
+    })
+);
+app.use(bodyParser.json());
+
+// DB Configuration
+const db = require("./config/keys").mongoURI;
+
+// Conect to MongoDB
+mongoose.
+    connect(
+        db,
+        { useNewUrlParser: true }
+    )
+    .then(() => console.log("MongoDB successfully connected"))
+    .catch(err => console.log(err));
+
+app.use(passport.initialize());
+
+require("./config/passport") (passport);
+
+app.use("/api/users", users)
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server up and running on port... ${port}`));
